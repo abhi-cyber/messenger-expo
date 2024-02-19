250c93db5b4f8f0b352f88278756882cdefe965a
abhi-cyber <abhirajchatrath@gmail.com>
2024-02-19 14:49:06 +0530

    message alignment error fixed

--- screens/ChatMessagesScreen.js
+++ screens/ChatMessagesScreen.js
@@ -270,7 +270,7 @@ const ChatMessagesScreen = () => {
                 onLongPress={() => handleSelectMessage(item)}
                 key={index}
                 style={[
-                  item?.senderId?._id == userId
+                  item?.senderId?._id == userId || item?.senderId == userId
                     ? {
                         alignSelf: "flex-end",
                         backgroundColor: "#DCF8C6",
